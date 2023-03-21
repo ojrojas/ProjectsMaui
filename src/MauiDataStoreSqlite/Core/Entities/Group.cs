ab1170e5b98864b6a456165672670558c842fa41
Oscar Rojas <orojasgarces1@gmail.com>
2023-03-20 20:50:44 -0500

    maui sqlite complete

--- src/MauiDataStoreSqlite/Core/Entities/Group.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiDataStoreSqlite.Core.Entities;
-
-[Table("Group")]
-public class Group: BaseEntity
-{
-    [MaxLength(250), Unique]
-    public string Name { get; set; }
-}
-
--- src/MauiDataStoreSqlite/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiDataStoreSqlite;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
